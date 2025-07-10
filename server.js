6a5dd4e6cfd79bdc65339aaca3db89208d14b89c
MOHD IMRAN <imranlaptop555@gmail.com>
2025-07-10 15:37:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// server.js
+
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const port = process.env.PORT || 3000
-const app = next({ dev: false })
+const port = parseInt(process.env.PORT, 10) || 3000
+const hostname = '0.0.0.0'
+
+const app = next({ dev: false, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(port, () => {
-    console.log(`🚀 Server running on http://localhost:${port}`)
+  }).listen(port, hostname, () => {
+    console.log(`🚀 Server running at http://0.0.0.0:${port}`)
   })
 })
